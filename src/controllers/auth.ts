b7d67764e9bfbffa1b621a7ac6cfffb353ec6f1b
chisomije92 <chisomije92@gmail.com>
2023-12-03 21:46:57 +0100

    comment out domain in cookie object

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -313,7 +313,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       sameSite: "lax",
-      domain: ".onrender.com"
+      // domain: ".onrender.com"
     })
     // .cookie(
     //   'refreshToken', refreshToken, {
